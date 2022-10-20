f68bc9de85091ae27fdd969ca408ac3e699c4d95
Paulo Rodrigues <paulo.robertojr33@gmail.com>
2022-10-20 15:14:12 -0300

    Instalação do material UI

---
+++ src/pages/_document.tsx
@@ -0,0 +1,40 @@
+import App from 'next/app'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { Component } from 'react'
+
+class MyDocument extends Document {
+    static async getInitialProps(ctx: any) {
+        const originalRenderPage = ctx.renderPage
+
+        // Run the React rendering logic synchronously
+        ctx.renderPage = () =>
+            originalRenderPage({
+                // Useful for wrapping the whole react tree
+                enhanceApp: (App: App) => App,
+                // Useful for wrapping in a per-page basis
+                enhanceComponent: (Component: Component) => Component,
+            })
+
+        // Run the parent `getInitialProps`, it now includes the custom `renderPage`
+        const initialProps = await Document.getInitialProps(ctx)
+
+        return initialProps
+    }
+
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+                    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        )
+    }
+}
+
+export default MyDocument
