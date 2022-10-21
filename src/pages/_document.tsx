2455620575081364c0266b2b76bdc8a48c7e583c
Paulo Rodrigues <prodrigues@iouu.com.br>
2022-10-21 12:17:49 -0300

    Cubo no centro da tela.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,23 +3,23 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { Component } from 'react'
 
 class MyDocument extends Document {
-    static async getInitialProps(ctx: any) {
-        const originalRenderPage = ctx.renderPage
+    // static async getInitialProps(ctx: any) {
+    //     const originalRenderPage = ctx.renderPage
 
-        // Run the React rendering logic synchronously
-        ctx.renderPage = () =>
-            originalRenderPage({
-                // Useful for wrapping the whole react tree
-                enhanceApp: (App: App) => App,
-                // Useful for wrapping in a per-page basis
-                enhanceComponent: (Component: Component) => Component,
-            })
+    //     // Run the React rendering logic synchronously
+    //     ctx.renderPage = () =>
+    //         originalRenderPage({
+    //             // Useful for wrapping the whole react tree
+    //             enhanceApp: (App: App) => App,
+    //             // Useful for wrapping in a per-page basis
+    //             enhanceComponent: (Component: Component) => Component,
+    //         })
 
-        // Run the parent `getInitialProps`, it now includes the custom `renderPage`
-        const initialProps = await Document.getInitialProps(ctx)
+    //     // Run the parent `getInitialProps`, it now includes the custom `renderPage`
+    //     const initialProps = await Document.getInitialProps(ctx)
 
-        return initialProps
-    }
+    //     return initialProps
+    // }
 
     render() {
         return (
