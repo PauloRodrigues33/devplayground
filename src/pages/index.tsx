2fb0c881a95f9264da774bafec2c0097f0e8995e
Paulo Rodrigues <prodrigues@iouu.com.br>
2022-10-21 16:59:45 -0300

    Atualização do cubo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,13 +12,39 @@ const Home: NextPage = () => {
     var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 2000);
     camera.position.z = 5;
     var renderer = new THREE.WebGLRenderer();
+    renderer.shadowMap.enabled = true;
+    renderer.shadowMap.type = THREE.PCFSoftShadowMap;
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.getElementById('3d')?.appendChild(renderer.domElement);
 
     var geometry = new THREE.BoxGeometry(1, 1, 1);
-    var material = new THREE.MeshBasicMaterial({color: '#f00', opacity: 1});
+    var material = new THREE.MeshStandardMaterial({ color: '#fff', opacity: 1 });
+    material.side = THREE.DoubleSide;
     var cube = new THREE.Mesh(geometry, material);
+    cube.castShadow = true;
     scene.add(cube);
+
+    var planeGeometry = new THREE.PlaneGeometry(100, 100);
+    var plane = new THREE.Mesh(planeGeometry, material);
+    plane.rotateX(90);
+    plane.position.setY(-15);
+    plane.receiveShadow = true;
+    plane.castShadow = true;
+    scene.add(plane);
+
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.2);
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+    directionalLight.castShadow = true;
+    directionalLight.shadow.mapSize.width = 512; // default
+    directionalLight.shadow.mapSize.height = 512; // default
+    directionalLight.shadow.camera.near = 0.5; // default
+    directionalLight.shadow.camera.far = 2000; // default
+    directionalLight.position.set(10, 10, 0);
+    scene.add(ambientLight);
+    scene.add(directionalLight);
+
+    // const helper = new THREE.CameraHelper(directionalLight.shadow.camera);
+    // scene.add(helper);
 
     const Update = () => {
       requestAnimationFrame(Update);
@@ -44,6 +70,7 @@ const Home: NextPage = () => {
       </Head>
       <main>
         <div id='3d'></div>
+        <div style={{ position: 'absolute', top: '0', left: '0', width: '100%', textAlign: 'center', fontSize: '32px', marginTop: '20px' }}>A cube rotating in your X, Y quaternions</div>
       </main>
     </div>
   )
